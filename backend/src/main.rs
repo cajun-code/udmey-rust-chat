34aec55526b022d6de830518aeec461ea43dfc82
Paris Liakos <paris.liakos@gmail.com>
2023-12-01 21:53:06 +0200

    Lecture 06 - Atomic usize and rocket state

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,9 +1,19 @@
-use rocket::futures::StreamExt;
-use rocket_ws::{WebSocket, Channel};
+use std::{sync::atomic::{AtomicUsize, Ordering}, collections::HashMap};
+use rocket::State;
+use rocket::futures::{StreamExt, stream::SplitSink};
+use rocket_ws::{WebSocket, Channel, stream::DuplexStream, Message};
+
+static USER_ID_COUNTER: AtomicUsize = AtomicUsize::new(1);
+
+#[derive(Default)]
+struct ChatRoom {
+    connections: HashMap<usize, SplitSink<DuplexStream, Message>>
+}
 
 #[rocket::get("/")]
-fn chat(ws: WebSocket) -> Channel<'static> {
+fn chat(ws: WebSocket, state: &State<ChatRoom>) -> Channel<'static> {
     ws.channel(move |stream| Box::pin(async move {
+        let user_id = USER_ID_COUNTER.fetch_add(1, Ordering::Relaxed);
         let (mut ws_sink, mut ws_stream) = stream.split();
         while let Some(message) = ws_stream.next().await {
             // let _ = ws_sink.send(message?).await;
@@ -19,6 +29,7 @@ async fn main() {
         .mount("/", rocket::routes![
             chat
         ])
+        .manage(ChatRoom::default())
         .launch()
         .await;
 }
