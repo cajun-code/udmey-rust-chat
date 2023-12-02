a8a20e5451a90b46682e13c264cf9da508199181
Paris Liakos <paris.liakos@gmail.com>
2023-12-02 12:44:40 +0200

    Lecture 08 - ChatRoom impls

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -10,31 +10,37 @@ struct ChatRoom {
     connections: Mutex<HashMap<usize, SplitSink<DuplexStream, Message>>>
 }
 
+impl ChatRoom {
+    pub async fn add(&self, id: usize, sink: SplitSink<DuplexStream, Message>) {
+        let mut conns = self.connections.lock().await;
+        conns.insert(id, sink);
+    }
+
+    pub async fn broadcast_message(&self, message: Message) {
+        let mut conns = self.connections.lock().await;
+        for (_id, sink) in conns.iter_mut() {
+            let _ = sink.send(message.clone()).await;
+        }
+    }
+
+    pub async fn remove(&self, id: usize) {
+        let mut conns = self.connections.lock().await;
+        conns.remove(&id);
+    }
+}
+
 #[rocket::get("/")]
 fn chat<'r>(ws: WebSocket, state: &'r State<ChatRoom>) -> Channel<'r> {
     ws.channel(move |stream| Box::pin(async move {
         let user_id = USER_ID_COUNTER.fetch_add(1, Ordering::Relaxed);
         let (ws_sink, mut ws_stream) = stream.split();
-        {
-            let mut conns = state.connections.lock().await;
-            conns.insert(user_id, ws_sink);
-        }
-
+        state.add(user_id, ws_sink).await;
 
         while let Some(message) = ws_stream.next().await {
-            {
-                let mut conns = state.connections.lock().await;
-                let msg = message?;
-                for (_id, sink) in conns.iter_mut() {
-                    let _ = sink.send(msg.clone()).await;
-                }
-            }
+            state.broadcast_message(message?).await;
         }
 
-        {
-            let mut conns = state.connections.lock().await;
-            conns.remove(&user_id);
-        }
+        state.remove(user_id).await;
 
         Ok(())
     }))
