6ec2739dcc10cc7a06d5d8b2c16d1a97e07de7c4
Paris Liakos <paris.liakos@gmail.com>
2023-12-03 20:52:36 +0200

    Lecture 17 - Broadcast user list

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,6 +1,7 @@
-use std::{sync::atomic::{AtomicUsize, Ordering}, collections::HashMap};
+use std::collections::HashMap;
+use std::sync::atomic::{AtomicUsize, Ordering};
 use chrono::Utc;
-use common::WebSocketMessage;
+use common::*;
 use rocket::{State, tokio::sync::Mutex};
 use rocket::futures::{SinkExt, StreamExt, stream::SplitSink};
 use rocket_ws::{WebSocket, Channel, stream::DuplexStream, Message};
@@ -20,17 +21,38 @@ impl ChatRoom {
     }
 
     pub async fn broadcast_message(&self, message: Message, author_id: usize) {
-        let chat_message = common::ChatMessage {
+        let chat_message = ChatMessage {
             message: message.to_string(),
             author: format!("User #{}", author_id),
             created_at: Utc::now().naive_utc(),
         };
         let websocket_message = WebSocketMessage {
-            message_type: common::WebSocketMessageType::NewMessage,
+            message_type: WebSocketMessageType::NewMessage,
             message: Some(chat_message),
             users: None,
         };
         let mut conns = self.connections.lock().await;
+        for (_id, sink) in conns.iter_mut() {
+            let _ = sink.send(
+                Message::Text(json!(websocket_message).to_string())
+            ).await;
+        }
+    }
+
+    pub async fn broadcast_user_list(&self) {
+        let mut conns = self.connections.lock().await;
+        let mut users = vec![];
+
+        for (id, _) in conns.iter() {
+            users.push(format!("User #{}", id));
+        }
+
+        let websocket_message = WebSocketMessage {
+            message_type: WebSocketMessageType::UsersList,
+            message: None,
+            users: Some(users),
+        };
+
         for (_id, sink) in conns.iter_mut() {
             let _ = sink.send(
                 Message::Text(json!(websocket_message).to_string())
@@ -50,12 +72,14 @@ fn chat<'r>(ws: WebSocket, state: &'r State<ChatRoom>) -> Channel<'r> {
         let user_id = USER_ID_COUNTER.fetch_add(1, Ordering::Relaxed);
         let (ws_sink, mut ws_stream) = stream.split();
         state.add(user_id, ws_sink).await;
+        state.broadcast_user_list().await;
 
         while let Some(message) = ws_stream.next().await {
             state.broadcast_message(message?, user_id).await;
         }
 
         state.remove(user_id).await;
+        state.broadcast_user_list().await;
 
         Ok(())
     }))
