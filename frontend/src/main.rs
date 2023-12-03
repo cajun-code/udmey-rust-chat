9c7d7a0bb75275905ee68988e0811d659a7c8052
Paris Liakos <paris.liakos@gmail.com>
2023-12-04 00:44:09 +0200

    Lecture 21 - Username change FE logic

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,4 +1,6 @@
-use common::{WebSocketMessage, WebSocketMessageType};
+use chrono::Utc;
+use common::{WebSocketMessage, WebSocketMessageType, ChatMessage};
+use serde_json::json;
 use yew::prelude::*;
 use yew_hooks::use_websocket;
 use crate::message_list::MessageList;
@@ -42,9 +44,31 @@ fn App() -> Html {
         }
     });
 
+    let cloned_username = username.clone();
     let cloned_ws = ws.clone();
     let send_message_callback = Callback::from(move |msg: String| {
-        cloned_ws.send(msg.clone());
+        let websocket_message = WebSocketMessage {
+            message_type: WebSocketMessageType::NewMessage,
+            message: Some(ChatMessage {
+                message: msg,
+                created_at: Utc::now().naive_utc(),
+                author: cloned_username.clone(),
+            }),
+            users: None,
+            username: None,
+        };
+        cloned_ws.send(json!(websocket_message).to_string());
+    });
+
+    let cloned_ws = ws.clone();
+    let change_username_callback = Callback::from(move |username: String| {
+        let websocket_message = WebSocketMessage {
+            message_type: WebSocketMessageType::UsernameChange,
+            message: None,
+            users: None,
+            username: Some(username),
+        };
+        cloned_ws.send(json!(websocket_message).to_string());
     });
 
     html! {
@@ -58,7 +82,13 @@ fn App() -> Html {
                 </div>
             </div>
             <div class="row">
-                <SendDialog send_message_callback={send_message_callback} username={username} />
+                if username.len() > 0 {
+                    <SendDialog
+                        change_username_callback={change_username_callback}
+                        send_message_callback={send_message_callback}
+                        username={username}
+                    />
+                }
             </div>
         </div>
     }
--- frontend/src/send_dialog.rs
+++ frontend/src/send_dialog.rs
@@ -1,9 +1,10 @@
-use web_sys::HtmlTextAreaElement;
+use web_sys::{HtmlTextAreaElement, HtmlInputElement};
 use yew::prelude::*;
 
 #[derive(Properties, PartialEq)]
 pub struct Props {
     pub send_message_callback: Callback<String>,
+    pub change_username_callback: Callback<String>,
     pub username: String,
 }
 
@@ -11,6 +12,10 @@ pub struct Props {
 pub fn send_dialog(props: &Props) -> Html {
     let new_message_handle = use_state(String::default);
     let new_message = (*new_message_handle).clone();
+    let new_username_handle = use_state(|| props.username.clone());
+    let new_username = (*new_username_handle).clone();
+    let is_editing_username_handle = use_state(bool::default);
+    let is_editing_username = (*is_editing_username_handle).clone();
 
     let cloned_new_message_handle = new_message_handle.clone();
     let on_message_change = Callback::from(move |e: Event| {
@@ -27,12 +32,50 @@ pub fn send_dialog(props: &Props) -> Html {
         new_message_handle.set("".to_string());
     });
 
+    let cloned_is_editing_handle = is_editing_username_handle.clone();
+    let on_username_change_click = Callback::from(move |_: MouseEvent| {
+        cloned_is_editing_handle.set(true);
+    });
+
+    let cloned_is_editing_handle = is_editing_username_handle.clone();
+    let on_username_edit_cancel_click = Callback::from(move |_: MouseEvent| {
+        cloned_is_editing_handle.set(false);
+    });
+
+    let callback = props.change_username_callback.clone();
+    let cloned_new_username = new_username.clone();
+    let on_username_apply_cancel_click = Callback::from(move |_: MouseEvent| {
+        callback.emit(cloned_new_username.clone());
+        is_editing_username_handle.set(false);
+    });
+
+    let on_username_change = Callback::from(move |e: Event| {
+        let target = e.target_dyn_into::<HtmlInputElement>();
+        if let Some(input) = target {
+            new_username_handle.set(input.value());
+        }
+    });
+
     html! {
         <div class="input-group">
-            <button class="btn btn-secondary">{props.username.clone()}</button>
+            if is_editing_username {
+                <input type="text" class="form-control" onchange={on_username_change} value={new_username} />
+                <button class="btn btn-secondary" onclick={on_username_apply_cancel_click}>
+                    {"✓"}
+                </button>
+                <button class="btn btn-danger" onclick={on_username_edit_cancel_click}>
+                    {"x"}
+                </button>
+            } else {
+                <button class="btn btn-secondary" onclick={on_username_change_click}>
+                    {props.username.clone()}
+                </button>
+            }
             <span class="input-group-text">{"Your message:"}</span>
             <textarea class="form-control" onchange={on_message_change} value={new_message}></textarea>
-            <button type="submit" class="btn-primary" onclick={on_button_click}>{"Send"}</button>
+            <button type="submit" class="btn-primary" onclick={on_button_click}>
+                {"Send"}
+            </button>
         </div>
     }
 }
