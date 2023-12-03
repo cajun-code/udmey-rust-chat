ea87182d49ad3772bc1dddbc8194396a957050f6
Paris Liakos <paris.liakos@gmail.com>
2023-12-03 15:33:30 +0200

    Lecture 14 - Backend JSON

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,7 +1,9 @@
 use std::{sync::atomic::{AtomicUsize, Ordering}, collections::HashMap};
+use chrono::Utc;
 use rocket::{State, tokio::sync::Mutex};
 use rocket::futures::{SinkExt, StreamExt, stream::SplitSink};
 use rocket_ws::{WebSocket, Channel, stream::DuplexStream, Message};
+use serde_json::json;
 
 static USER_ID_COUNTER: AtomicUsize = AtomicUsize::new(1);
 
@@ -16,10 +18,17 @@ impl ChatRoom {
         conns.insert(id, sink);
     }
 
-    pub async fn broadcast_message(&self, message: Message) {
+    pub async fn broadcast_message(&self, message: Message, author_id: usize) {
+        let chat_message = common::ChatMessage {
+            message: message.to_string(),
+            author: format!("User #{}", author_id),
+            created_at: Utc::now().naive_utc(),
+        };
         let mut conns = self.connections.lock().await;
         for (_id, sink) in conns.iter_mut() {
-            let _ = sink.send(message.clone()).await;
+            let _ = sink.send(
+                Message::Text(json!(chat_message).to_string())
+            ).await;
         }
     }
 
@@ -37,7 +46,7 @@ fn chat<'r>(ws: WebSocket, state: &'r State<ChatRoom>) -> Channel<'r> {
         state.add(user_id, ws_sink).await;
 
         while let Some(message) = ws_stream.next().await {
-            state.broadcast_message(message?).await;
+            state.broadcast_message(message?, user_id).await;
         }
 
         state.remove(user_id).await;
